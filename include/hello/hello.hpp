a56dcbf0fe5f05da3c8195d7f4a561748124569f
Hyunsu Cho <phcho@nvidia.com>
2020-05-09 00:48:29 -0700

    Revert "Add buffer protocol interface example"
    This reverts commit 0cd361095ab0e00385c6bcbcdf9717713a95f21c.

--- include/hello/hello.hpp
+++ include/hello/hello.hpp
@@ -1,5 +1,4 @@
 #include <string>
-#include <vector>
 
 extern "C" {
 void hello(const char* name);
@@ -7,21 +6,3 @@ int multiply(int a, int b);
 }
 
 std::string concat(const std::string& a, const std::string& b);
-
-struct PyBufferInterface1D {
-  void* buf;
-  char* format;
-  size_t itemsize;
-  size_t nitem;
-};
-
-class MyObj {
- private:
-  std::vector<double> field_;
-  const char* format;
- public:
-  MyObj() : format("d") {}
-  ~MyObj() = default;
-  void FillToN(int n);
-  PyBufferInterface1D GetBufferInterface();
-};
--- python/hello/hello.pyx
+++ python/hello/hello.pyx
@@ -4,63 +4,11 @@
 # cython: language_level = 3
 
 from libcpp.string cimport string
-from libcpp.memory cimport unique_ptr
-
-import numpy as np
 
 cdef extern from "hello/hello.hpp":
     cdef void hello(const char* name) except +
     cdef int multiply(int a, int b) except +
     cdef string concat(const string& a, const string& b) except +
-
-    cdef cppclass PyBufferInterface1D:
-        void* buf
-        char* format
-        size_t itemsize
-        size_t nitem
-
-    cdef cppclass MyObj:
-        void FillToN(int n) except +
-        PyBufferInterface1D GetBufferInterface() except +
-
-cdef class MyObjWrapper:
-    cdef unique_ptr[MyObj] _handle
-    cdef Py_ssize_t shape[1]
-    cdef Py_ssize_t strides[1]
-
-    def __cinit__(self):
-        self._handle.reset(new MyObj())
-
-    def __dealloc__(self):
-        self._handle.release()
-
-    def __getbuffer__(self, Py_buffer* buffer, int flags):
-        cdef PyBufferInterface1D interface = self._handle.get().GetBufferInterface()
-        cdef Py_ssize_t itemsize = interface.itemsize
-
-        self.shape[0] = interface.nitem
-        self.strides[0] = itemsize
-
-        buffer.buf = interface.buf
-        buffer.format = interface.format
-        buffer.internal = NULL
-        buffer.itemsize = itemsize
-        buffer.len = interface.nitem * itemsize
-        buffer.ndim = 1
-        buffer.obj = self
-        buffer.readonly = 0
-        buffer.shape = self.shape
-        buffer.strides = self.strides
-        buffer.suboffsets = NULL
-
-    def __releasebuffer__(self, Py_buffer* buffer):
-        pass
-
-    def fill_to_n(self, n):
-        self._handle.get().FillToN(n)
-
-def multiply_python(a, b):
-    return multiply(a, b)
 
 def example():
     s = 'Philip Cho'
@@ -68,10 +16,5 @@ def example():
     cdef string new_name = concat('John '.encode('utf-8'), 'Doe'.encode('utf-8'))
     hello(new_name.c_str())
 
-    print('Demonstrate buffer interface for std::vector<double>')
-    obj = MyObjWrapper()
-    print('  Fill std::vector<double> with {0, 1, ... 12}')
-    obj.fill_to_n(12)
-    print('  Take buffer interface of std::vector<double> and feed it into NumPy')
-    x = np.asarray(obj)
-    print(f'  Success! The NumPy array has value {x} (dtype={x.dtype})')
+def multiply_python(a, b):
+    return multiply(a, b)
